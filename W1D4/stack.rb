fd6476bb6df4710a29554daf7ddf46ee98d0e3a7
Kenta Kodama <kentakodama@gmail.com>
2017-08-03 23:54:20 -0700

    initial commit

---
+++ W1D4/stack.rb
@@ -0,0 +1,29 @@
+
+class Stack
+    def initialize
+      # create ivar to store stack here!
+      @stack = []
+    end
+
+    def add(el)
+      # adds an element to the stack
+      @stack << el
+    end
+
+    def remove
+      # removes one element from the stack
+      @stack.pop if @stack.length >= 1
+    end
+
+    def show
+      # return a copy of the stack
+      @stack
+    end
+end
+
+
+if $0 == __FILE__
+
+  test = Stack.new
+
+end
