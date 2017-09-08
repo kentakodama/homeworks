e74e1f8848fd3d4db8c122930ce4e0ee2be78247
Kenta Kodama <kentakodama@gmail.com>
2017-09-08 09:35:47 -0700

    added hw

---
+++ W6D5/skeleton/calculator.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    // your code here
+    this.state = { result: 0, num1: '', num2: '' };
+    this.setNum1 = this.setNum1.bind(this);
+    this.setNum2 = this.setNum2.bind(this);
+
+  }
+  setNum1(e) {
+  // your code here
+  }
+  // your code here
+  setNum2(e) {
+  // your code here
+  }
+  // your code here
+
+
+  render() {
+   return (
+     <div>
+       <input></input>
+       <h1>{this.state.result}</h1>
+     </div>
+   );
+ }
+}
+
+export default Calculator;
